ad3934d44783c7994d7d8b217214e5eecfb93910
Anhad <84476084+07anhad@users.noreply.github.com>
2022-03-07 23:02:32 +0530

    done

---
+++ index.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('.container');
+const yesBtn = document.querySelector('.yesBtn');
+const noBtn = document.querySelector('.noBtn');
+
+noBtn.addEventListener('click', () => {
+    noBtn.style.position = 'absolute';
+
+    const randomLeft = Math.floor(Math.random() * container.clientWidth);
+    const randomTop = Math.floor(Math.random() * container.clientHeight);
+
+    
+    if (randomLeft >= container.clientWidth / 2) {
+        noBtn.style.left = `${randomLeft - noBtn.clientWidth}px`;
+        
+    }
+
+    if (randomTop >= container.clientHeight / 2) {
+        noBtn.style.top = `${randomTop - noBtn.clientHeight}px`;
+
+    }
+
+
+});
+
+yesBtn.addEventListener('click', () => {
+    container.innerHTML = `<h2> LOL 😂 <br> I knew it!</h2>`
+})
